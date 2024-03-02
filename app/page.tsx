ca4ce5a7359f0e9477005ed178cd94028c1c4910
dinogomez <dinogomez117@gmail.com>
2024-03-02 14:39:24 +0800

    update: fixes

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,10 @@ export default function Home() {
     <main className="flex h-full flex-col items-center justify-center px-12 pt-36 xs:p-36 space-y-2">
       <LandingCard title={siteConfig.title} desc={siteConfig.desc} />
       <div className="bg-slate-500 font-mono text-sm text-gray-200 p-3 shadow rounded-md flex flex-col">
-        <span>
+        <span className="flex flex-col">
           {"Auth/Register is working, via Supabase/Prisma/Iron-Session"}{" "}
+          <span>{"- Create/Update Orders in Courier View"}</span>
+          <span>{"- View/Recieve Orders in User View"}</span>
         </span>
         <div className="flex flex-col text-gray-300/70">
           <span>{"//note: Registerd User Credentials"} </span>
--- commons/lib/routes/index.ts
+++ commons/lib/routes/index.ts
@@ -1,4 +1,4 @@
-export const publicRoutes = ["/", "/courier", "/api"];
+export const publicRoutes = ["/", "/courier", "/api", "/ruri"];
 
 export const authRoutes = ["/auth/login", "/auth/register"];
 
